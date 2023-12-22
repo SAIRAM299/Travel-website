af65ad772fdcf6d8adf63e2340691a4aa50a1748
SAIRAM299 <psairam299gmail.com>
2023-12-22 19:14:21 +0530

    new

--- src/component/Offer/Offer.jsx
+++ src/component/Offer/Offer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Offer/Offer.css';
 import { MdKingBed } from "react-icons/md";
 import { MdBathtub } from "react-icons/md";
@@ -6,8 +6,38 @@ import { FaWifi } from "react-icons/fa";
 import { TbAirConditioning } from "react-icons/tb";
 import { MdLocationOn } from "react-icons/md";
 import { BsArrowRightShort } from "react-icons/bs";
-import img6 from "../../Assets/godvalley.jpg";
+import img7 from "../../Assets/marine.webp";
+import img8 from "../../Assets/impel.jpg";
+import img9 from "../../Assets/godvalley.jpg";
 const Offer = () => {
+
+    const [off, setoff] = useState([
+        {
+            id: 1,
+            img: img7,
+            desti: 'Marine Fort',
+            locat: 'Grand Line',
+            price: '$8000',
+            att:  'image for website'
+        },
+        {
+            id: 2,
+            img: img8,
+            desti: 'Impel Down',
+            locat: 'Calm Belt',
+            price: '$9000',
+            att:  'image for website'
+        },
+        {
+            id: 3,
+            img: img9,
+            desti: 'Thriller Back',
+            locat: 'East Blue',
+            price: '$10000',
+            att:  'image for website'
+        }
+    ])
+
     return (
         <div>
             <section className='offer contains sec'>
@@ -19,65 +49,74 @@ const Offer = () => {
                             Special Offers
                         </h2>
                         <p>
-                            fxhdhgdcghdchgchngcnhcnhgfgjdcgjcjgncfjgnd cjgdjggxg ncgxgh
+                        "Set sail for the grand line, where dreams and adventures await beyond every horizon."
                         </p>
                     </div>
 
 
                     <div className="maincontent grid">
 
-                        <div className='offers'>
+                        {
+                            off.map((dd,i) => (
 
-                            <div className='destimg'>
-                            <img src={img6} />
-                            <span className='discount'>
-                                30% Off
-                            </span>
-                            </div>
+                                <div className='offers' key={i}>
 
-                        <div className="offerbody">
-                            <div className="price flex">
-                                <h4>
-                                    $1000
-                                </h4>
-                            <span className='status'>
-                                for rent
-                                </span>    
-                            </div>
-                       
-                       <div className="ammy flex">
+                                    <div className='destimg'>
+                                        <img src={dd.img} alt={dd.att}/>
+                                        <span className='discount'>
+                                            30% Off
+                                        </span>
+                                    </div>
 
-                        <div className="singleammy flex">
-                        <MdKingBed className='icon'/>
-                        <small>2 Beds</small>
-                        </div>
-                        <div className="singleammy flex">
-                        <MdBathtub className='icon'/>
-                        <small>1 Bath</small>
-                        </div>
-                        <div className="singleammy flex">
-                        <FaWifi  className='icon'/>
-                        <small>WI-FI</small>
-                        </div>
-                        <div className="singleammy flex">
-                        <TbAirConditioning className='icon'/>
-                        <small>Airconditon</small>
-                        </div>
+                                    <div className="offerbody">
+                                        <div className="price flex">
+                                            <h4>
+                                                {dd.price}
+                                            </h4>
+                                            <span className='status'>
+                                                for rent
+                                            </span>
+                                        </div>
 
-                       </div>
+                                        <div className="ammy flex">
 
-<div className="location flex">
-<MdLocationOn className='icon'/>
-<small>Marine Fort , Grand Line </small>
-</div>
+                                            <div className="singleammy flex">
+                                                <MdKingBed className='icon' />
+                                                <small>2 Beds</small>
+                                            </div>
+                                            <div className="singleammy flex">
+                                                <MdBathtub className='icon' />
+                                                <small>1 Bath</small>
+                                            </div>
+                                            <div className="singleammy flex">
+                                                <FaWifi className='icon' />
+                                                <small>WI-FI</small>
+                                            </div>
+                                            <div className="singleammy flex">
+                                                <TbAirConditioning className='icon' />
+                                                <small>Airconditon</small>
+                                            </div>
 
-<button className='btn flex'>
-    View Details
-    <BsArrowRightShort/>
-</button>
-                        </div>
-                   
-                    </div>
+                                        </div>
+
+                                        <div className="location flex">
+                                            <MdLocationOn className='icon' />
+                                            <small>{dd.desti} , {dd.locat} </small>
+                                        </div>
+
+                                        <button className='btn flex'>
+                                            View Details
+                                            <BsArrowRightShort />
+                                        </button>
+                                    </div>
+
+                                </div>
+
+
+                            ))
+                        }
+
+
                     </div>
                 </div>
             </section>
